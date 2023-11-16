446d92ba9886e10a0d838cd83bd43b30a16c3659
unknown <elevation2610@gmail.com>
2023-11-16 15:18:28 +0100

    first commit

---
+++ contact.js
@@ -0,0 +1,15 @@
+//  3 + 2;
+//  if (3 + 2 === 5){;
+//  	prompt("What do you want?");
+// } else{
+// 	alert("good");
+// };
+function multiply(a,b){
+if (a <10 && b < 10){
+ 	return "YOU ARE WELCOME!";	
+} else{
+	return a * b;
+	}
+}
+alert(multiply(7,6));
+// prompt('Enter your name: ');
